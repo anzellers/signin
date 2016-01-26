ad7b534b17e6b6a9c38a13e25d15c0020ab262d3
Graphic <graphic@SOD-SJH-605-10M.local>
2016-01-26 17:09:30 -0500

    Adding events

--- js/main.js
+++ js/main.js
@@ -1,1 +1,44 @@
-//Some jQuery Stuff
+//Functions
+
+
+var openWindow = function() {
+	$('.modal').fadeIn(1000);
+}
+
+var closeWindow = function() {
+	$('.modal').fadeOut(1000);
+}
+
+// var submitError = function() {
+// 	$('input').addClass('error');
+// 	event.stopPropagation();
+// }
+
+var removeError = function() {
+		$(this).removeClass('error');
+}
+
+$('.modal').on('click', closeWindow);
+
+$('.signin').on('click', openWindow);
+
+$('.close').on('click', closeWindow);
+
+// $('.submit').on('click', submitError);
+
+// $('input[name="user"]').on('focus', removeError);
+
+$('input[name="pass"]').on('click', function(event) {
+	event.stopPropagation();
+	$('input[name="pass"]').removeClass('error');
+});
+
+$('input[name="user"]').on('click', function(event) {
+	event.stopPropagation();
+	$('input[name="user"]').removeClass('error');
+});
+
+$('.submit').on('click', function(event) {
+	event.stopPropagation();
+	$('input').addClass('error');
+});
